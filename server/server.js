69e95ff53d89b69ac6aacb7c543cbef869cdc49b
hhrh <hardykenta@icloud.com>
2025-01-19 17:09:20 -0800

    changes

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const authRouter = require('./routes/auth/auth-routes')
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
 const shopListingRouter = require("./routes/shop/listing-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
-const shopAddressRouter = require('./routes/shop/address-routes')
-const OrderRouter = require('./routes/shop/order-routes')
-const adminOrderRouter = require('./routes/admin/order-routes')
-const shopSearchRouter = require('./routes/shop/search-routes')
-const shopReviewsRouter = require('./routes/shop/reviews-routes')
-const commonFeatureRouter = require('./routes/common/feature-routes')
-const adminUserRouter = require('./routes/admin/user-routes')
+const shopAddressRouter = require("./routes/shop/address-routes");
+const OrderRouter = require("./routes/shop/order-routes");
+const adminOrderRouter = require("./routes/admin/order-routes");
+const shopSearchRouter = require("./routes/shop/search-routes");
+const shopReviewsRouter = require("./routes/shop/reviews-routes");
+const commonFeatureRouter = require("./routes/common/feature-routes");
+const adminUserRouter = require("./routes/admin/user-routes");
 //connect to DB
 mongoose
     .connect(process.env.MONGO_URL)
@@ -36,22 +36,22 @@ app.use(
         credentials: true,
     })
 );
-  
+
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/admin/orders", adminOrderRouter);
-app.use('/api/shop/products', shopListingRouter);
-app.use('/api/shop/cart', shopCartRouter);
-app.use('/api/shop/address', shopAddressRouter);
-app.use('/api/shop/order', OrderRouter);
-app.use('/api/shop/search', shopSearchRouter);
-app.use('/api/shop/reviews', shopReviewsRouter);
-app.use('/api/common/feature', commonFeatureRouter);
-app.use('/api/admin/users', adminUserRouter);
+app.use("/api/shop/products", shopListingRouter);
+app.use("/api/shop/cart", shopCartRouter);
+app.use("/api/shop/address", shopAddressRouter);
+app.use("/api/shop/order", OrderRouter);
+app.use("/api/shop/search", shopSearchRouter);
+app.use("/api/shop/reviews", shopReviewsRouter);
+app.use("/api/common/feature", commonFeatureRouter);
+app.use("/api/admin/users", adminUserRouter);
 app.get("/", (req, res) => {
-       res.send("API is running...");
+    res.send("API is running...");
 });
 app.get("/api/hello", (req, res) => {
     res.json({ message: "Hello from the backend!" });
